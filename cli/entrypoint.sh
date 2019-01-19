8be2fe2ddf99ffc4c84f3e816247359344963e80
davidk <kdavid@gmail.com>
2019-01-19 12:26:05 -0800

    actions/docker/cli: Allow multiple commands to be executed
    This is a mirror of the functionality in bin/sh where multiple
    commands can be passed:
    
    `args = ["build -t myimage .", "tag myimage myimage:nightly"]
    
    Signed-off-by: davidk <kdavid@gmail.com>

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -l
-
-sh -c "docker $*"
+for cmd in "$@"; do
+    echo "Running '$cmd'..."
+    if sh -c "docker $cmd"; then
+        # no op
+        echo "Successfully ran '$cmd'"
+    else
+        exit_code=$?
+        echo "Failure running '$cmd', exited with $exit_code"
+        exit $exit_code
+    fi
+done
